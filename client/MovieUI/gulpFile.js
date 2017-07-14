c2d4cf0c6b59af3ed6e8ca00aadb34159f9c833d
bobbykhanna <iceboxbob@gmail.com>
2017-07-14 16:13:09 -0700

    feat(module-client): Gulp Automation Complete

--- client/MovieUI/gulpFile.js
+++ client/MovieUI/gulpFile.js
@@ -2,7 +2,12 @@ var gulp = require('gulp');
 var clean = require('gulp-clean');
 var inject = require('gulp-inject');
 var browsersync = require('browser-sync').create();
-var angularFilesort = require('gulp-angular-filesort');
+var angularFileSort = require('gulp-angular-filesort');
+var filter = require('gulp-filter');
+var concat = require('gulp-concat');
+var cleanCss = require('gulp-clean-css');
+var uglify = require('gulp-uglify');
+var merge = require('merge-stream');
 var bowerFiles = require('main-bower-files');
 
 
@@ -20,27 +25,59 @@ gulp.task('clean', function () {
         .pipe(clean());
 });
 
+gulp.task('minifyCss', function () {
+
+    var vendorStyles = gulp.src(bowerFiles())
+        .pipe(filter(['**/*.css']))
+        .pipe(concat('vendor.min.css'))
+        .pipe(cleanCss({debug: true, compatibility: 'ie8'}))
+        .pipe(gulp.dest(config.paths.build + '/styles'));
+
+    var appStyles = gulp.src(config.paths.src + '/**/*.css')
+        .pipe(concat('app.min.css'))
+        .pipe(cleanCss({debug: true, compatibility: 'ie8'}))
+        .pipe(gulp.dest(config.paths.build + '/styles'));
+
+    return merge(vendorStyles, appStyles);
+});
+
+gulp.task('minifyJs', function () {
+
+    var vendorScripts = gulp.src(bowerFiles())
+        .pipe(filter(['**/*.js']))
+        .pipe(concat('vendor.min.js'))
+        .pipe(uglify())
+        .pipe(gulp.dest(config.paths.build + '/scripts'));
+
+    var appScripts = gulp.src(config.paths.src + '/**/*.js')
+        .pipe(angularFileSort())
+        .pipe(concat('app.min.js'))
+        .pipe(uglify())
+        .pipe(gulp.dest(config.paths.build + '/scripts'));
+
+    return merge(vendorScripts, appScripts);
+});
 
 
 gulp.task('inject', function () {
 
-        var cssFiles = gulp.src([
-            config.paths.src + '/**/*.css'
-        ], {read: false});
+    var cssFiles = gulp.src([
+        config.paths.src + '/**/*.css'
+    ], {read: false});
 
-        var jsFiles = gulp.src([
-            config.paths.src + '/**/*.js'
-        ]);
+    var jsFiles = gulp.src([
+        config.paths.src + '/**/*.js'
+    ]);
 
-        return gulp.src(config.paths.src + '/index.html')
-            .pipe(inject(gulp.src(bowerFiles(), {read: false}), {name: 'bower'}))
-            .pipe(inject(cssFiles, {ignorePath: 'src', addRootSlash: false}))
-            .pipe(inject(jsFiles.pipe(angularFilesort()), {ignorePath: 'src', addRootSlash: false}))
-            .pipe(gulp.dest(config.paths.build));
+    return gulp.src(config.paths.src + '/index.html')
+        .pipe(inject(gulp.src(bowerFiles(), {read: false}), {name: 'bower'}))
+        .pipe(inject(cssFiles, {ignorePath: 'src', addRootSlash: false}))
+        .pipe(inject(jsFiles.pipe(angularFileSort()), {ignorePath: 'src', addRootSlash: false}))
+        .pipe(gulp.dest(config.paths.build));
 
 });
 
-gulp.task('serve',['inject'], function () {
+gulp.task('serve', ['inject'], function () {
 
     browsersync.init({
 
@@ -55,4 +92,45 @@ gulp.task('serve',['inject'], function () {
             config.paths.src + '/**/*'
         ]
     });
+
+
+});
+
+gulp.task('htmls', function () {
+
+    return gulp.src([config.paths.src + '/**/*html', '!' + config.paths.src + '/index.html'])
+        .pipe(gulp.dest(config.paths.build));
+
+});
+
+gulp.task('fonts', function () {
+
+    return gulp.src(bowerFiles())
+        .pipe(filter(['**/*.{eot.ttf,woff,woff2}']))
+        .pipe(gulp.dest(config.paths.build + '/fonts'));
+
+});
+
+gulp.task('others', function () {
+    return gulp.src([config.paths.src + '/**/*.*', '!**/*.html', '!**/*.css', '!**/*.js'])
+        .pipe(gulp.dest(config.paths.build));
+});
+
+gulp.task('build', ['minifyCss', 'minifyJs', 'htmls', 'fonts', 'others'], function () {
+
+    var vendorFiles = gulp.src([
+        config.paths.build + '/styles/vendor.min.css',
+        config.paths.build + '/scripts/vendor.min.js'
+    ], {read: false});
+
+    var appFiles = gulp.src([
+        config.paths.build + '/styles/app.min.css',
+        config.paths.build + '/scripts/app.min.js'
+    ], {read: false});
+
+    return gulp.src(config.paths.src + '/index.html')
+        .pipe(inject(vendorFiles, {name: 'vendor', ignorePath: 'build', addRootSlash: false}))
+        .pipe(inject(appFiles, {name: 'app', ignorePath: 'build', addRootSlash: false}))
+        .pipe(gulp.dest(config.paths.build));
+
 });
